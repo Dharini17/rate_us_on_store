990659bf6e373eea198db0805d91424e46c42159
dharini <lad.dharini@gmail.com>
2024-04-14 07:29:16 +0530

    v-0.0.2

--- lib/rate_us_on_store.dart
+++ lib/rate_us_on_store.dart
@@ -1,21 +1,25 @@
 library rate_us_on_store;
+
 import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
 
 class RateUsOnStore {
-
   ///android package name
   String? androidPackageName;
+
   ///Appstore app id
   String? appstoreAppId;
 
-  RateUsOnStore({required this.androidPackageName,required this.appstoreAppId});
+  RateUsOnStore(
+      {required this.androidPackageName, required this.appstoreAppId});
 
   launch() async {
-    String strURL = Platform.isAndroid ? "https://play.google.com/store/apps/details?id=$androidPackageName" :
-    "https://itunes.apple.com/it/app/$appstoreAppId?mt=8";
+    String strURL = Platform.isAndroid
+        ? "https://play.google.com/store/apps/details?id=$androidPackageName"
+        : "https://itunes.apple.com/it/app/$appstoreAppId?mt=8";
 
-    if (await launchUrl(Uri.parse(strURL),mode: LaunchMode.externalApplication)) {
+    if (await launchUrl(Uri.parse(strURL),
+        mode: LaunchMode.externalApplication)) {
       await canLaunchUrl(Uri.parse(strURL));
     }
   }
